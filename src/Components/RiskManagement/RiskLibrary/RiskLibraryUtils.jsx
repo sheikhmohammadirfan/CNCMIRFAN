68f1f78b531fda42f6fef2dd4abfe936beaf5827
DeWill404 <mr.irshad.siddique@gmail.com>
2024-10-12 23:37:59 +0530

    fix

--- src/Components/RiskManagement/RiskLibrary/RiskLibraryUtils.jsx
+++ src/Components/RiskManagement/RiskLibrary/RiskLibraryUtils.jsx
@@ -2,8 +2,7 @@ import { Typography, makeStyles } from "@material-ui/core";
 import columnToCellMap from "../../../assets/data/RiskManagement/TableColCellMap";
 import { HEADER_TABLE_NAME_MAP } from "../../../assets/data/RiskManagement/RiskLibrary/LibraryColumns";
 
-export const useStyle = makeStyles(theme => ({
-
+export const useStyle = makeStyles((theme) => ({
   // Root Register Container
   libraryContainer: {
     maxHeight: `calc(100vh - ${theme.headerHeight}px)`,
@@ -23,7 +22,7 @@ export const useStyle = makeStyles(theme => ({
 
   libraryCaption: {
     opacity: 0.75,
-    fontSize: "0.875rem"
+    fontSize: "0.875rem",
   },
 
   // Style for table container
@@ -63,34 +62,34 @@ export const useStyle = makeStyles(theme => ({
         fontFamily: "Material Icons",
         right: 0,
         fontSize: 18,
-        width: 10
+        width: 10,
       },
       "&::before": {
         content: "'\\2193'",
         color: theme.palette.grey[400],
-        display: 'flex',
+        display: "flex",
         top: 8,
-        right: 3
+        right: 3,
       },
       "&::after": {
         content: "'\\2191'",
         color: theme.palette.grey[400],
         top: 8,
-        right: 10
+        right: 10,
       },
       "&.asc::before": {
         content: "'\\2193'",
-        color: '#4477CE'
+        color: "#4477CE",
       },
       "&.dsc::after": {
         content: "'\\2191'",
-        color: '#4477CE'
+        color: "#4477CE",
       },
     },
 
     // Giving box shadow on left side to First cell on select
     "& tr.Mui-selected td:nth-child(1)": {
-      boxShadow: 'inset 4px 0 0 0 #4477CE',
+      boxShadow: "inset 4px 0 0 0 #4477CE",
     },
 
     // Change background color of selected row CELLS
@@ -103,7 +102,9 @@ export const useStyle = makeStyles(theme => ({
     // "& thead th:nth-child(1)": { background: "#cce6e3" },
     "& thead th:nth-child(2)": { borderRight: `1px solid #d9d9d9` },
     // "& tbody td:nth-child(1)": { borderRight: `1px solid #d9d9d9` },
-    "& tbody td:nth-child(2)": { borderRight: `1px solid ${theme.palette.grey[300]}` },
+    "& tbody td:nth-child(2)": {
+      borderRight: `1px solid ${theme.palette.grey[300]}`,
+    },
 
     // searched data cell style
     "& tbody td[data-searched='true']": { border: "2px solid #4477CE" },
@@ -114,7 +115,7 @@ export const useStyle = makeStyles(theme => ({
     color: "#797979",
     fontSize: 12,
     textTransform: "uppercase",
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
 
   // Style to make all cell height of 3 line
@@ -131,8 +132,7 @@ export const useStyle = makeStyles(theme => ({
     border: "1px solid rgba(0, 0, 0, 0.2)",
     borderRadius: "50px",
   },
-
-}))
+}));
 
 /* Header cell component */
 export const HeaderCell = ({ text }) => {
@@ -151,7 +151,7 @@ export const RowCell = ({ text }) => {
     <Typography variant="body2" className={classes.tableCell}>
       {text}
     </Typography>
-  )
+  );
 };
 
 export const mapDataToHeader = (columns, sorting, updateSort) => ({
@@ -159,16 +159,24 @@ export const mapDataToHeader = (columns, sorting, updateSort) => ({
     text,
     params: {
       onClick: text === "Categories" ? () => {} : () => updateSort(text),
-      className: sorting && sorting.sort_by === HEADER_TABLE_NAME_MAP[text] ? (sorting.sort_order === 1 ? 'asc' : 'dsc') : "",
+      className:
+        sorting &&
+        (HEADER_TABLE_NAME_MAP[text] === "description"
+          ? sorting.sort_by === "scenario"
+          : sorting.sort_by === HEADER_TABLE_NAME_MAP[text])
+          ? sorting.sort_order === 1
+            ? "asc"
+            : "dsc"
+          : "",
     },
   })),
   cellStyle: {
     fontWeight: "bold",
     cursor: "pointer",
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
   },
-})
+});
 
 export const generateRows = (
   data,
@@ -189,13 +197,7 @@ export const generateRows = (
   // Generate row of POA&M table
   for (let i = 0; i < rowCount; i++)
     rowData.push({
-      data: mapDataToRow(
-        data[i],
-        i,
-        columns,
-        matchedCell,
-        categories,
-      ),
+      data: mapDataToRow(data[i], i, columns, matchedCell, categories),
       props: {
         selected: selectedList.indexOf(i) !== -1,
       },
@@ -207,48 +209,52 @@ export const generateRows = (
     rowData,
     rowStyle: { cursor: "pointer" },
     cellStyle: {
-      display: 'flex',
-      alignItems: 'center',
+      display: "flex",
+      alignItems: "center",
       position: "relative",
     },
   };
-}
+};
 
-const mapDataToRow = (row, rowIndex, columns, matchedCell, categories) => (
-  columns.map(colName => {
-    let CellComponent = colName in columnToCellMap && columnToCellMap[colName]
+const mapDataToRow = (row, rowIndex, columns, matchedCell, categories) =>
+  columns.map((colName) => {
+    let CellComponent = colName in columnToCellMap && columnToCellMap[colName];
     let cellValue = getCellValue(row, colName, categories);
     return {
-      text: colName in columnToCellMap ? <CellComponent cellValue={cellValue} /> : <RowCell text={cellValue} />,
+      text:
+        colName in columnToCellMap ? (
+          <CellComponent cellValue={cellValue} />
+        ) : (
+          <RowCell text={cellValue} />
+        ),
       params:
         colName === "Id"
           ? {
-            "sticky": "",
-            "data-searched": Boolean(
-              matchedCell.find(
-                (cell) =>
-                  cell.column === colName &&
-                  cell.row === rowIndex &&
-                  cell.selected === true
-              )
-            ),
-            tabIndex: 0,
-          }
+              sticky: "",
+              "data-searched": Boolean(
+                matchedCell.find(
+                  (cell) =>
+                    cell.column === colName &&
+                    cell.row === rowIndex &&
+                    cell.selected === true
+                )
+              ),
+              tabIndex: 0,
+            }
           : {
-            "data-searched": Boolean(
-              matchedCell.find(
-                (cell) =>
-                  cell.column === colName &&
-                  cell.row === rowIndex &&
-                  cell.selected === true
-              )
-            ),
-            tabIndex: 0,
-          },
-    }
-  })
-)
+              "data-searched": Boolean(
+                matchedCell.find(
+                  (cell) =>
+                    cell.column === colName &&
+                    cell.row === rowIndex &&
+                    cell.selected === true
+                )
+              ),
+              tabIndex: 0,
+            },
+    };
+  });
 
 const getCellValue = (row, colName, categories) => {
-  return row[colName]
-}
+  return row[colName];
+};
